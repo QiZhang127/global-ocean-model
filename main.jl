b1424000fb1813442a1c66a9dd3d8fec9a7eb7bf
Lucas Gloege <ljg48@login1.bouchet.ycrc.yale.edu>
2026-07-31 17:53:48 -0400

    fix: bugs in main script

--- main.jl
+++ main.jl
@@ -21,6 +21,9 @@ MPI.Init()
 
 @info "Import necesary modules ..."
 
+# TODO: cleanup the imports in the main.jl
+# not all of these necessary in the top-level .jl file
+# and be specific where functions are coming from
 using Oceananigans
 using NumericalEarth
 using OceanBioME
@@ -65,6 +68,8 @@ include("src/Closures.jl")
 include("src/Atmosphere.jl")
 include("src/Initialization.jl")
 include("src/Output.jl")
+include("src/Callback.jl")
+include("src/Land.jl")
 
 using .GridSetup
 using .BiogeochemistrySetup
@@ -73,38 +78,70 @@ using .ClosureSetup
 using .AtmosphereSetup
 using .Initialization
 using .OutputSetup
+using .CallbackSetup
+using .LandSetup
 
 # dates to run the simulation
 dates = DateTime(2000,1,1):Month(1):DateTime(2005,12,31)
 
 grid = build_grid(arch)
 
-bgc, bcs, pco2, CO₂_flux1, CO₂_flux2 = build_bgc(grid)
+(; biogeochemistry, boundary_conditions, CO₂_flux1, CO₂_flux2) = build_bgc(grid)
 
 forcing = build_alkalinity_forcing(
     grid;
-    amplitude = 1.0, # amplitude of the release
+    amplitude = 1.0,      # amplitude of the release
     lon       = 236.5425, # longitude location of release
     lat       = 48.1292,  # latitude location of release
-    sigma     = 5.0, # standard deviation of the patch (in pixels)
-    ti        = 1.0, # day to start release
-    tf        = 2.0. # day to end release
+    sigma     = 5.0,      # standard deviation of the patch (in pixels)
+    ti        = 1.0,      # day to start release
+    tf        = 2.0      # day to end release
 )
 
-closure = build_closure()
+# ---------------------------------------------------------------------
+# Land
+# TODO: should land, atmosphere, radiation, ... all other forcings
+#       be combined into a PrescribedForcing.jl module?
+# ---------------------------------------------------------------------
+(; land) = build_land(arch)
 
-atmosphere, radiation = build_atmosphere(arch, dates)
+# ---------------------------------------------------------------------
+# atmosphere
+# ---------------------------------------------------------------------
+(; atmosphere, radiation) = build_atmosphere(arch, dates)
+
+# ---------------------------------------------------------------------
+# ocean simulation
+# ---------------------------------------------------------------------
+# define tracers
+physics_tracers=(:T, :S,)
+bgc_tracers = (:NO₃, :NH₄, :P, :Z, :sPOM, :bPOM, :DOM, :DIC1, :DIC2, :Alk1, :Alk2)
+tracers = (physics_tracers..., bgc_tracers...)
+
+closure = build_closure()
+free_surface       = SplitExplicitFreeSurface(grid; substeps=70)
+momentum_advection = WENOVectorInvariant(order=7)
+tracer_advection   = WENO(order=7)
 
 ocean = ocean_simulation(
     grid;
-    closure,
+    momentum_advection,
+    tracer_advection,
+    free_surface,
+    tracers,
     forcing,
-    biogeochemistry = bgc,
-    boundary_conditions = bcs
-)
+    closure,
+    biogeochemistry,
+    boundary_conditions
+    )
+
+@show ocean.model
 
 initialize_ocean!(ocean, grid, first(dates))
 
+# ---------------------------------------------------------------------
+# coupled simulation
+# ---------------------------------------------------------------------
 coupled_model = OceanOnlyModel(
         ocean;
         atmosphere,
@@ -117,6 +154,8 @@ simulation = Simulation(
         stop_time = 365days
     )
 
-configure_output!(ocean, grid, pco2, CO₂_flux1, CO₂_flux2)
+add_progress_callback!(simulation)
+
+configure_output!(ocean, grid)
 
 run!(simulation)
